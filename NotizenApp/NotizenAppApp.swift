483a4f83c26e44891fddd6b3d09fc58abd9ce07a
fiesematente <mosesfinimento@googlemail.com>
2024-11-04 16:51:01 +0100

    Initial Commit

---
+++ NotizenApp/NotizenAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotizenAppApp.swift
+//  NotizenApp
+//
+//  Created by Mike Finimento on 04.11.24.
+//
+
+import SwiftUI
+
+@main
+struct NotizenAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
